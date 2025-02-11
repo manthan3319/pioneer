898c0f07e01c67a93644438df162a3d7e0ffa2b9
manthan <vaghasiyamanthan8@gmail.com>
2025-02-11 18:59:16 +0530

    add loader

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
 import Copyright_Section from './components/Copyright_Section/Copyright_Section';
-import AboutUs from './components/AboutUs/AboutUs';
-import Page_Services from './components/Services/Page_Services';
-import Client from './components/Client/Client';
-// import Machinery from './components/Machinery/Machinery';
-import Gallery from './components/Gallery/Gallery';
-import Contactus from './components/Contactus/Contactus';
+import Loader from './components/Loader/Loader';
+
+// Lazy load route components
+const Home = lazy(() => import('./components/Home/Home'));
+const AboutUs = lazy(() => import('./components/AboutUs/AboutUs'));
+const Page_Services = lazy(() => import('./components/Services/Page_Services'));
+const Client = lazy(() => import('./components/Client/Client'));
+const Gallery = lazy(() => import('./components/Gallery/Gallery'));
+const Contactus = lazy(() => import('./components/Contactus/Contactus'));
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // This effect will run once when the component mounts
+    // We're not changing the loading state, so it will always stay true
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<AboutUs />} />
-        <Route path="services" element={<Page_Services />} />
-        <Route path="clients" element={<Client />} />
-        {/* <Route path="machinery" element={<Machinery />} /> */}
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="contactus" element={<Contactus />} />
-      </Routes>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<AboutUs />} />
+            <Route path="services" element={<Page_Services />} />
+            <Route path="clients" element={<Client />} />
+            <Route path="gallery" element={<Gallery />} />
+            <Route path="contactus" element={<Contactus />} />
+          </Routes>
+        </Suspense>
+      )}
       <Copyright_Section/>
     </BrowserRouter>
   );
