34de55f74ef6e60bdefc9220b9f66a6c4076861c
manthan <vaghasiyamanthan8@gmail.com>
2025-02-18 10:51:17 +0530

    chnage laziy loader

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React, { Suspense, lazy, useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Copyright_Section from './components/Copyright_Section/Copyright_Section';
-import Loader from './components/Loader/Loader';
-
-// Lazy load route components
-const Home = lazy(() => import('./components/Home/Home'));
-const AboutUs = lazy(() => import('./components/AboutUs/AboutUs'));
-const Page_Services = lazy(() => import('./components/Services/Page_Services'));
-const Client = lazy(() => import('./components/Client/Client'));
-const Gallery = lazy(() => import('./components/Gallery/Gallery'));
-const Contactus = lazy(() => import('./components/Contactus/Contactus'));
-
+import React, { Suspense, lazy, useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Copyright_Section from "./components/Copyright_Section/Copyright_Section";
+import Home from "./components/Home/Home";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Page_Services from "./components/Services/Page_Services";
+import Client from "./components/Client/Client";
+import Gallery from "./components/Gallery/Gallery";
+import Contactus from "./components/Contactus/Contactus";
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="services" element={<Page_Services />} />
-            <Route path="clients" element={<Client />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="contactus" element={<Contactus />} />
-          </Routes>
-      <Copyright_Section/>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<AboutUs />} />
+        <Route path="services" element={<Page_Services />} />
+        <Route path="clients" element={<Client />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="contactus" element={<Contactus />} />
+      </Routes>
+      <Copyright_Section />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
